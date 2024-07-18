dbc08f791cfab9fae303f6d0be1f85d9da169203
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-07-18 10:57:14 -0300

    Commit Integração com a API 02

--- src/main/java/br/com/lima/chatbots/TestaIntegracao.java
+++ src/main/java/br/com/lima/chatbots/TestaIntegracao.java
@@ -18,7 +18,8 @@ public class TestaIntegracao {
         var user = "Gere 05 Perguntas sobre Matemática";
         var system = "Matemática é uma área do conhecimento que inclui os tópicos dos números, fórmulas e estruturas relacionadas, formas e os espaços em que estão contidos, e quantidades e suas mudanças. ";
 
-        var service = new OpenAiService("your_token");
+        var chave = System.getenv("OPENIA_API_KEY");
+        var service = new OpenAiService(chave);
 
         var completionRequest = ChatCompletionRequest
                 .builder()
@@ -31,7 +32,7 @@ public class TestaIntegracao {
         service
                 .createChatCompletion(completionRequest)
                 .getChoices()
-                .forEach(System.out::println);
+                .forEach(c -> System.out.print(c.getMessage().getContent()));
 
     }
 }
