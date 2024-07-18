a66fcd668f1d5254a0a653c5e888a18a998dd5ab
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-07-17 21:01:22 -0300

    Commit Inicial02

--- src/main/java/br/com/lima/chatbots/TestaIntegracao.java
+++ src/main/java/br/com/lima/chatbots/TestaIntegracao.java
@@ -1,7 +1,12 @@
+/*
+  By Sósthenes Oliveira Lima
+ */
 package br.com.lima.chatbots;
 
 public class TestaIntegracao {
     public static void main(String[] args) {
 
+        var user = "Gere 05 Perguntas sobre Matemática";
+        var system = "Matemática é uma área do conhecimento que inclui os tópicos dos números, fórmulas e estruturas relacionadas, formas e os espaços em que estão contidos, e quantidades e suas mudanças. "
     }
 }
