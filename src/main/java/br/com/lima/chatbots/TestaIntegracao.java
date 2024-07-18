d8f3cf88d0c9185a6076d24c8727f396d1ee200d
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-07-18 10:26:15 -0300

    Commit Integração com a API 01

--- src/main/java/br/com/lima/chatbots/TestaIntegracao.java
+++ src/main/java/br/com/lima/chatbots/TestaIntegracao.java
@@ -3,10 +3,35 @@
  */
 package br.com.lima.chatbots;
 
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TestaIntegracao {
     public static void main(String[] args) {
 
         var user = "Gere 05 Perguntas sobre Matemática";
-        var system = "Matemática é uma área do conhecimento que inclui os tópicos dos números, fórmulas e estruturas relacionadas, formas e os espaços em que estão contidos, e quantidades e suas mudanças. "
+        var system = "Matemática é uma área do conhecimento que inclui os tópicos dos números, fórmulas e estruturas relacionadas, formas e os espaços em que estão contidos, e quantidades e suas mudanças. ";
+
+        var service = new OpenAiService("your_token");
+
+        var completionRequest = ChatCompletionRequest
+                .builder()
+                .model("gpt-4")
+                .messages(Arrays.asList(
+                        new ChatMessage(ChatMessageRole.USER.value(), user),
+                        new ChatMessage(ChatMessageRole.SYSTEM.value(),system)
+                ))
+                .build();
+        service
+                .createChatCompletion(completionRequest)
+                .getChoices()
+                .forEach(System.out::println);
+
     }
 }
